ae4d85fd288c4ca3bbe67f4e545fad0a14ff2e13
IAimPikaZ <iaimpikaz@gmail.com>
2023-08-11 13:42:05 +0300

    Add: welcome message popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,43 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import RadioPlayer from './RadioPlayer';
 import SongTitleFetcher from './SongTitleFetcher';
 import ArtworkFetcher from './ArtworkFetcher';
 import logo from '../src/assets/logo.png';
-import Favorites from './Favorites'; // Import the Favorites component
+import Favorites from './Favorites';
+import Popup from './Popup'; // Import the Popup component
 
 import './App.css';
 
 const App: React.FC = () => {
   const [radioUrl, setRadioUrl] = useState('');
   const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [showPopup, setShowPopup] = useState(true); // State for showing the popup
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setRadioUrl(event.target.value);
   };
 
+  const closePopup = () => {
+    localStorage.setItem('popupDismissed', 'true');
+    setShowPopup(false);
+  };
+
+  useEffect(() => {
+    const dismissed = localStorage.getItem('popupDismissed');
+    if (dismissed) {
+      setShowPopup(false);
+    }
+  }, []);
+
   return (
     <div>
+      {showPopup && <Popup onClose={closePopup} />} {/* Render the popup */}
       <div className="artwork-background">
         {/* The ArtworkFetcher component will display the artwork image */}
         <ArtworkFetcher radioUrl={radioUrl} />
       </div>
       <div className="input-container">
-      <Favorites setRadioUrl={setRadioUrl} />
+        <Favorites setRadioUrl={setRadioUrl} />
         <input
           type="text"
           placeholder="Enter radio URL"
---
+++ src/Popup.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import logo from '../src/assets/logo.png';
+import './Popup.css';
+
+interface PopupProps {
+  onClose: () => void;
+}
+
+const Popup: React.FC<PopupProps> = ({ onClose }) => {
+  const closePopup = () => {
+    localStorage.setItem('popupDismissed', 'true');
+    onClose();
+  };
+
+  return (
+    <div className="popup-container">
+      <div className="popup-content">
+        <img src={logo} alt="Logo" className="logo" />
+        <h1>Welcome to DoogFM - Your Ultimate Online Radio Player!</h1>
+        <p>
+  <strong>🎶 Introducing DoogFM: Elevate Your Radio Experience 🎶</strong>
+</p>
+<p>
+  Welcome to DoogFM – your ultimate hub for hassle-free internet radio streaming! 📻✨
+</p>
+<p>
+  Imagine a world where you can effortlessly enjoy your favorite internet radio stations with just a few clicks.
+  Say goodbye to the endless sites that doesn't let you put custom radio stations – DoogFM has got you covered!
+</p>
+<p>
+  🌟 <strong>Seamless Streaming:</strong> Dive into a world of music from across the globe.
+  Enter radio station URLs, and let the melodies transport you to new musical dimensions.
+</p>
+<p>
+  ⭐ <strong>Create Your Playlist:</strong> Love a station? Save it to your personalized favorites list!
+  Your preferred tunes are now just a tap away. No more typing, just enjoying.
+</p>
+<p>
+  🎨 <strong>Immersive Experience:</strong> DoogFM is more than just music. It brings the magic of album covers,
+  artist names, and song titles to your fingertips. Immerse yourself in the details of your favorite tracks.
+</p>
+<p>
+  🚫 <strong>Ad-Free Pleasure:</strong> We believe in uninterrupted enjoyment.
+  DoogFM is free from pesky ads that break the rhythm of your music. Your tunes, your way!
+</p>
+<p>
+  💙 <strong>Support the Beat:</strong> While DoogFM is ad-free, you can still lend a hand by using the code "zerotwo"
+  on Salad.com or showing your love on Ko-fi. Every bit helps us keep the music flowing!
+</p>
+<p>
+  Join the DoogFM community today and embark on a musical journey like never before.
+  Whether you're discovering new tracks or revisiting old favorites, DoogFM is here to amplify your radio experience.
+  Listen, save, and explore – all with the click of a button. 🎧🌐
+</p>
+
+        <div className="buttons">
+          <a href="https://ko-fi.com/yukiokoito">Ko-fi</a>
+          <a href="https://github.com/OugiFormula">GitHub</a>
+          <a href="https://discord.gg/vEXGQudVcg">Discord</a>
+          <a href="https://doog.cool/">Website</a>
+        </div>
+        <button className="close-button" onClick={closePopup}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
